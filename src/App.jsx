7b3d33ac362b4bb7989cea7cb0b87f956977b8b5
bhumi-rastogi <bhumi.rastogi05@gmail.com>
2023-12-27 14:51:43 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,78 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useReducer } from 'react'
+import { useRef } from 'react'
+
+
+let list = [{
+  id : 'Good Morning',
+  see : true
+}]
+
+function task(x, y){
+  switch (y.type) {
+    case 'Increment':
+      return[...x, {id: y.ta, see:true}]
+      
+    case 'Change_Theme':
+      return x.map((a,index)=>{
+        if(y.index === index){
+          return {...a , see : !a.see};
+        }else{
+          return a
+        }
+      })
+  
+    default:
+      return x;
+      break;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [work, remove] = useReducer(task,list);
+  let [temp, setTemp] = useState('')
+  let references = useRef(null)
+
+
+  function Change(e){
+    setTemp(e.target.value);
+  }
+
+  function Key(){
+    remove({type:'Increment', ta : temp })
+  }
+
+  function Change_Theme(index){
+    remove({type:'Change_Theme', index})
+  }
+
+  function This(){
+    references.current.focus();
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='box'>
+
+      <input type="text" onChange={(e) => Change(e)} ref ={references} />
+
+      <button onClick={Key}>
+        Addition
+      </button>
+      {work.map((a,index) =>{
+          return(
+            <div key={index}>
+              {(a.see) ? a.id : "Hidden"}
+              <button onClick={()=> Change_Theme(index)}>Hide</button>
+              
+              {(a.see) ? <button onClick={This}>Focus</button> : null}
+            </div>
+          )
+        })
+      }  
+    </div> 
   )
 }
 
